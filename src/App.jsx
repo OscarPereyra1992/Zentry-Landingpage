343e130154b360c331ade4dfb13340a5a900cf56
OscarPereyra1992 <oscarpereyra.mhs@gmail.com>
2025-01-07 18:33:42 -0300

    Changes in Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Features from "./components/Features";
-import { Footer } from "./components/Footer";
-import Hero from "./components/hero";
+import Footer  from "./components/Footer";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Story from "./components/Story";
 
